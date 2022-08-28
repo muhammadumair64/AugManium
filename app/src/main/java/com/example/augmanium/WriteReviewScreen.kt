116de6546da4e88bf1e0d3d64581330929424a72
muhammadumair64 <84626460+muhammadumair64@users.noreply.github.com>
2022-08-28 12:42:04 +0500

    ui changes

---
+++ app/src/main/java/com/example/augmanium/WriteReviewScreen.kt
@@ -0,0 +1,11 @@
+package com.example.augmanium
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WriteReviewScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_write_review_screen)
+    }
+}
